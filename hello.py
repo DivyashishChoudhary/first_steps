60308e0cc697939f2bb9f48d3a119c3d0ff6153a
Divyashish Choudhary <divyashishchoudhary1@gmail.com>
2026-04-22 12:11:37 +0530

    feature-b: formal welcome

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
-print("Welcome to Divyashish's first repo!")
+print("Hello and welcome to Divyashish's repository!")
 print("Hello,World ")
 print("Day 1 of learning to be a better than average coder")
