1b0d89058332d7b1cdc40f1d81f7152619370839
DivyashishChoudhary <divyashishchoudhary1@gmail.com>
2026-04-24 16:04:39 +0530

    Add calculator, new_hello and playground scripts (#2)

---
+++ playground.py
@@ -0,0 +1,9 @@
+# Ask user for their name
+print("What's your name?")
+name = input()
+name = name.strip().title() # Remove whitespace and capitalize name
+
+#Say hello to user
+print(f"Hello, {name}")
+
+
